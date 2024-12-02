12d4b83926b4f120471abf32eb280c49eee26c0f
carlasiles1 <carlasiles.19cs@gmail.com>
2024-12-02 09:17:41 +0100

    ejemplos js añadidos

---
+++ var-const.js
@@ -0,0 +1,40 @@
+// Symbol//
+let uniqueId = symbol ("id")
+let hiddenKey = Symbol("hidden")
+
+//Null//
+let selectedProduct = null;
+
+//Undefined//
+let name;
+
+// string//
+let Username = "Carla Siles"
+
+//function//
+let greet = function(name) {
+    return `Hola, ${name}!`;
+};
+
+//Number//
+let age = 29
+
+//Number decimal//
+let height = 1.60
+
+//Array//
+let lenguajes = ["HTML","CSS","JavaScript"]
+
+// Boolean//
+let isStudent = "true"
+let isTeacher = "false"
+
+//Objetc//
+let myCv = {
+   profession :"full-Stack Developer",
+   year :"2025"
+};
+
+//Const//
+const PlanB = BigInt (200000000)
+
