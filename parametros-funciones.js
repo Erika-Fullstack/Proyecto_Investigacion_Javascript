643290949f3d5f4ae0080beec7efa81df7f3d09b
Erika-Fullstack <erika.full.stack@gmail.com>
2024-12-03 11:35:54 +0100

    cambios del style e index y parametros

---
+++ parametros-funciones.js
@@ -0,0 +1,8 @@
+/* Parámetros básicos */
+function saludar(nombre) {
+    console.log(`¡Hola, ${nombre}!`);
+}
+
+saludar("Marta"); // Salida: ¡Hola, Marta!
+saludar("Carlos"); // Salida: ¡Hola, Carlos!
+
