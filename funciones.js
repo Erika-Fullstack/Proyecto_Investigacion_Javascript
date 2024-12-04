6ba892b898f5eab59abecfb2ff17b67c08ee9167
Gaizka M <gaizkamg@gmail.com>
2024-12-04 17:45:50 +0100

    arreglo funciones y parametros-funciones

--- funciones.js
+++ funciones.js
@@ -1,65 +1,73 @@
 // FUNCIONES NOMINATIVAS //
 
 // Ejemplo 1: Declaración de una función con nombre
-function saludar() {
-    console.log("¡Hola, mundo!");
-  }
+    function saludar() {
+        console.log("¡Hola, mundo!");
+      }
 
-// Llamamos a la función
-saludar(); // Salida: ¡Hola, mundo!
+    // Llamamos a la función
+    saludar(); // Salida: ¡Hola, mundo!
   
 
 // Ejemplo 2: Función con parámetros
-function sumar(a, b) {
-    return a + b;
-  }
-  
-  const resultado = sumar(3, 7);
+    function sumar(a, b) {
+        return a + b;
+      }
+      
+      const resultado = sumar(3, 7);
  // console.log(resultado); // Salida: 10 
 
 
  // Ejemplo 3: Función que devuelve un mensaje personalizado
- function presentar(nombre) {
-    return `Hola, me llamo ${nombre}.`;
-  }
+    function presentar(nombre) {
+        return `Hola, me llamo ${nombre}.`;
+      }
 
 // Ejemplo 4: Uso de una función nominativa dentro de otra función
-function usarSuma(a, b) {
-  return sumar(a, b); // Llama a la función 'sumar' dentro de 'usarSuma'
-}
+    function operacionMatematica(a, b, operacion) {
+      return operacion(a, b); 
+      // Llama a la función pasada como argumento
+    }
 
-// Función nominativa para sumar dos números
-function sumar(x, y) {
-  return x + y;
-}
+    // Función nominativa para sumar
+    function sumar(x, y) {
+      return x + y;
+    }
 
-// Usamos 'usarSuma' con la función 'sumar'
-//console.log(usarSuma(5, 3));  8
+    // Función nominativa para multiplicar
+    function multiplicar(x, y) {
+      return x * y;
+    }
+
+    // Usamos las funciones nominativas dentro de otra función
+    console.log(operacionMatematica(5, 3, sumar));       // 8
+    console.log(operacionMatematica(5, 3, multiplicar)); // 15
+
 
 // Ejemplo 5: Uso de una función nominativa dentro de un objeto
-const calculadora = {
-    sumar: function(a, b) {
-        return a + b;
-    },
-    restar: function(a, b) {
-        return a - b;
-    }
-  };
+    const calculadora = {
+        sumar: function(a, b) {
+            return a + b;
+        },
+        restar: function(a, b) {
+            return a - b;
+        }
+      };
 
 //Ejemplo 6: Uso de una función nominativa como métodos dentro de las clases
-class Persona {
-    constructor(nombre) {
-        this.nombre = nombre;
-    }
-      
-    saludar() {
-        return `Hola, soy ${this.nombre}`;
-    }
-  }
+    class Persona {
+        constructor(nombre) {
+            this.nombre = nombre;
+        }
+          
+        saludar() {
+            return `Hola, soy ${this.nombre}`;
+        }
+      }
 
-const persona = new Persona("Agatha");
-// console.log(persona.saludar()); 
-// Salida: Hola, soy Carlos
+    const persona = new Persona("Agatha");
+    // console.log(persona.saludar()); 
+    // Salida: Hola, soy Agatha
 
 
 //ANÓNIMAS Ejemplo: 2
